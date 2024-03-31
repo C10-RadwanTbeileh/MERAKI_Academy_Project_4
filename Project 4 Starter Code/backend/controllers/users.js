027780f9b9e77ff4ff4f06d6ae73d4fd9befd6e2
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-03-31 21:13:50 +0300

    modefy login and set token

--- Project 4 Starter Code/backend/controllers/users.js
+++ Project 4 Starter Code/backend/controllers/users.js
@@ -1,4 +1,6 @@
 const usersModel = require("../models/users");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const register = (req, res) => {
   const {
@@ -47,24 +49,46 @@ const register = (req, res) => {
 };
 
 const login = (req, res) => {
-  const { email, password } = req.body;
-
+  const {email} = req.body.email.toLowerCase();
+  const {password} = req.body;
   usersModel
     .findOne({ email: email })
-    .then((result) => {
-      if (result && result.password === password) {
-        res.status(201).json({
+    .populate("role","_id")
+    .then(async (result) => {
+      if (!result) {
+        return res.status(403).json({
+          success: false,
+          message: `The email doesn't exist or The password you’ve entered is incorrect`,
+        });
+      } try {
+        const valid = await bcrypt.compare(password, result.password);
+        if (!valid) {
+          return res.status(403).json({
+            success: false,
+            message: `The email doesn't exist or The password you’ve entered is incorrect`,
+          });
+        }
+        const payload = {
+          userId: result._id,
+          userName : result.firstName,
+          role: result.role,
+          country: result.country,
+        };
+
+        const options = {
+          expiresIn: "60m",
+        };
+        const token = jwt.sign(payload, process.env.SECRET, options);
+        res.status(200).json({
           success: true,
           message: `Valid login credentials`,
+          token: token,
         });
-      } else {
-        res.status(401).json({
-          success: false,
-          message: "Invalid login credentials",
-        });
+      }catch (error) {
+        throw new Error(error.message);
       }
     })
-    .catch((err) => {
+  .catch((err) => {
       res.status(500).json({
         success: false,
         message: `Server Error`,
--- Project 4 Starter Code/backend/models/users.js
+++ Project 4 Starter Code/backend/models/users.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String },
@@ -12,9 +12,9 @@ const userSchema = new mongoose.Schema({
   role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
 });
 
-// userSchema.pre("save", async function () {
-//   this.email = this.email.toLowerCase();
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
+userSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 module.exports = mongoose.model("User", userSchema);
