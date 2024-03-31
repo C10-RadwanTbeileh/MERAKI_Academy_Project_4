aac9d2a1cbbb0fa14514073ebf4243fd225ff92d
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-03-31 20:42:54 +0300

    make login function and router

--- Project 4 Starter Code/backend/controllers/users.js
+++ Project 4 Starter Code/backend/controllers/users.js
@@ -1,8 +1,16 @@
 const usersModel = require("../models/users");
 
-
 const register = (req, res) => {
-  const { firstName, lastName, age, country, email, password , phonNumber } = req.body;
+  const {
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    password,
+    phonNumber,
+    role,
+  } = req.body;
   const user = new usersModel({
     firstName,
     lastName,
@@ -11,7 +19,7 @@ const register = (req, res) => {
     email,
     password,
     role,
-    phonNumber
+    phonNumber,
   });
 
   user
@@ -20,27 +28,52 @@ const register = (req, res) => {
       res.status(201).json({
         success: true,
         message: `Account Created Successfully`,
-        author: result
+        author: result,
       });
     })
     .catch((err) => {
       if (err.keyPattern) {
         return res.status(409).json({
           success: false,
-          message: `The email already exists`
+          message: `The email already exists`,
         });
       }
       res.status(500).json({
         success: false,
         message: `Server Error`,
-        err: err.message
+        err: err.message,
       });
     });
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
 
-
+  usersModel
+    .findOne({ email: email })
+    .then((result) => {
+      if (result && result.password === password) {
+        res.status(201).json({
+          success: true,
+          message: `Valid login credentials`,
+        });
+      } else {
+        res.status(401).json({
+          success: false,
+          message: "Invalid login credentials",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
 
 module.exports = {
-  register
+  register,
+  login,
 };
