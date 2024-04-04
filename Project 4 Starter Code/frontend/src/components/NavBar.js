04e9e94eb435982d0438140662ef81746df9fb78
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-04-04 22:12:33 +0300

    NavBar element

--- Project 4 Starter Code/frontend/src/components/NavBar.js
+++ Project 4 Starter Code/frontend/src/components/NavBar.js
@@ -8,7 +8,20 @@ const NavBar = () => {
 
 
 
-   
+    return (
+        <div className='Home'>
+            <img src=''/>
+<button>Home</button>
+ <button>Categories</button> {/* list */}
+<button>LogIn</button>
+<button>Register</button>
+
+
+        </div>
+    )
+
+
+       
 
 
 
