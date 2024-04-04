eacc56d762a39fc7d3fe94b11457128185e7fd8a
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-04-04 22:14:47 +0300

    login component

--- Project 4 Starter Code/frontend/src/components/Login.js
+++ Project 4 Starter Code/frontend/src/components/Login.js
@@ -8,7 +8,47 @@ import { UserContext } from "../../App";
 
 const Login = () => {
 
-   
+    const [massage, setMassage] = useState("");
+    const navigate = useNavigate();
+    const { setIsLoggedIn, setToken } = useContext(UserContext);
+
+
+const [email , setEmail]= useState();
+const [password ,setPassword]= useState();
+
+
+
+    return (
+        <div>
+            
+<input type='email' placeholder='Email' onChange={(e)=>{
+setEmail(e.target.value);
+}}/>
+
+<input type='password' placeholder='Password' onChange={(e)=>{
+setPassword(e.target.value);
+}} />
+
+<button onClick={()=>{
+axios
+.post("http://localhost:5000/users/login", {
+  email,
+  password,
+})
+.then((result) => {
+  navigate("/Home")
+  setIsLoggedIn(true);
+  setToken(result.data.token);
+  localStorage.setItem("token",result.data.token)
+})
+.catch((error) => {
+  setMassage(error.response.data.message);
+});
+}}> LogIN </button>
+{massage}
+        </div>
+    )
+
 }
 
 export default Login
