87871b48d30879dd8675e2f39034e73382138050
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-04-04 22:13:28 +0300

    home element

--- Project 4 Starter Code/frontend/src/components/Home.js
+++ Project 4 Starter Code/frontend/src/components/Home.js
@@ -9,7 +9,11 @@ const Home = () => {
   return (
     <div>Home
 
-
+<img src=''/>
+<div className='Chicken'>Chicken</div>
+<div className='SeaFood'>SeaFood</div>
+<div className='Meat'>Meat</div>
+<div className='Fruits'>Fruits</div>
     </div>
   
   
